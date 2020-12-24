fc217b3af8cefd9088eff0b6822f6ee0f592cfc0
Sergey Sedelnikov <sergey.sedelnikov@yahoo.com>
2020-12-24 13:43:27 +0200

    more tests for initial state

--- NetState.Tests/InitialStateRun.cs
+++ NetState.Tests/InitialStateRun.cs
@@ -2,18 +2,20 @@ using System;
 using Xunit;
 using XStateNet;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace NetState.Tests
 {
-    public class UnitTest1
+    public class InitialStateRun
     {
         [Fact]
-        public void OnenterActionsExecuted()
+        public void OneInitialStateOneOnEnterActionExecuted()
         {
             bool onEnterActionRun = false;
 
             var state = new State("My test");
-            state.WithActionOnEnter(() => {
+            state.WithActionOnEnter(() =>
+            {
                 onEnterActionRun = true;
             });
 
@@ -29,6 +31,125 @@ namespace NetState.Tests
             Task.Delay(2000).GetAwaiter().GetResult();
 
             Assert.True(onEnterActionRun);
+        }
+
+        [Fact]
+        public void OneInitialStateManyOnEnterActionExecuted()
+        {
+            int onEnterActionRun = 0;
+
+            var state = new State("My test");
+            state.WithActionOnEnter(() =>
+            {
+                Console.WriteLine("1");
+                onEnterActionRun += 1;
+            }).WithActionOnEnter(() =>
+            {
+                Console.WriteLine("2");
+                onEnterActionRun += 1;
+            }).WithActionOnEnter(() =>
+            {
+                Console.WriteLine("3");
+                onEnterActionRun += 1;
+            });
+
+            var stateMachine = new StateMachine("test", "test", "My test");
+            stateMachine.States = new State[]{
+                state
+            };
+
+            var interpreter = new Interpreter();
+            interpreter.StartStateMachine(stateMachine);
+
+            // TODO: wait until state machine is done
+            Task.Delay(2000).GetAwaiter().GetResult();
+
+            Assert.Equal(3, onEnterActionRun);
+        }
+
+        [Fact]
+        public void OneInitialStateOneServiceExecuted()
+        {
+            bool serviceExecuted = false;
+
+            var state = new State("My test");
+            state.WithInvoke((state, callback) =>
+            {
+                serviceExecuted = true;
+            });
+
+            var stateMachine = new StateMachine("test", "test", "My test");
+            stateMachine.States = new State[]{
+                state
+            };
+
+            var interpreter = new Interpreter();
+            interpreter.StartStateMachine(stateMachine);
+
+            // TODO: wait until state machine is done
+            Task.Delay(2000).GetAwaiter().GetResult();
+
+            Assert.True(serviceExecuted);
+        }
+
+        [Fact]
+        public void NoInitialStateGiven()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                var state = new State("My test 1");
+
+                var stateMachine = new StateMachine("test", "test", "My test");
+                stateMachine.States = new State[]{
+                state
+            };
+
+                var interpreter = new Interpreter();
+                interpreter.StartStateMachine(stateMachine);
+            });
+        }
+
+        [Fact]
+        public void OneInitialStateMultipleServicesExecuted()
+        {
+            object lockObject = new object();
+            int serviceExecuted = 0;
+            int thread1 = 0;
+            int thread2 = 0;
+
+            var state = new State("My test");
+            state.WithInvoke((state, callback) =>
+            {
+                // executed in parallel
+                thread1 = Thread.CurrentThread.ManagedThreadId;
+                lock(lockObject)
+                {
+                    serviceExecuted += 1;
+                }
+            })
+            .WithInvoke((state, callback) =>
+            {
+                // executed in parallel
+                thread2 = Thread.CurrentThread.ManagedThreadId;
+                lock(lockObject)
+                {
+                    serviceExecuted += 1;
+                }
+            });
+
+            var stateMachine = new StateMachine("test", "test", "My test");
+            stateMachine.States = new State[]{
+                state
+            };
+
+            var interpreter = new Interpreter();
+            interpreter.StartStateMachine(stateMachine);
+
+            // TODO: wait until state machine is done
+            Task.Delay(2000).GetAwaiter().GetResult();
+
+            Assert.Equal(2, serviceExecuted);
+            Assert.NotEqual(thread1, thread2);
         }
     }
 }
