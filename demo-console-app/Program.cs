16ac9bbed956f22d6ea655bd235c804aff2e9ef0
Sergey Sedelnikov <sergey.sedelnikov@yahoo.com>
2021-01-06 18:35:12 +0200

    documentation*
    *

--- demo-console-app/Program.cs
+++ demo-console-app/Program.cs
@@ -8,18 +8,18 @@ namespace demo_console_app
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            
+
             Console.WriteLine("Demo application to demonstrate the state machien engine");
-            
+
             // ==================== TRAFFIC LIGHT DEMO ============================
             TrafficLight.TrafficLightDemoRunner.Run();
 
             // ==================== MQTT DEMO ============================
             //MqttDemo.MqttDemoRunner.Run();
-            
+
             // never exit the program
             while (true)
             {
@@ -28,6 +28,35 @@ namespace demo_console_app
                 Console.ReadLine();
             }
 
+        }
+
+        void Demo()
+        {
+            // create state. Each state has a unique ID
+            State myState1 = new State("myState1");
+            State myState2 = new State("myState2");
+            State myState3 = new State("myState3");
+            State myState4 = new State("myState4");
+
+            // create state machine
+            // provide ID, name, and initial state ID
+            StateMachine machine = new StateMachine("myMachine1", "My Machine", "myState1");
+            // fill up states
+            machine.States = new []{
+                myState1, myState2, myState3, myState4
+            };
+
+            Interpreter interpreter = new Interpreter(machine);
+            interpreter.StartStateMachine();
+            // you can subscribe for state changed event
+            interpreter.OnStateChanged += (sender, args) => {
+                Console.WriteLine("Current state: " + args.State.Id);
+                Console.WriteLine("Previous state: " + args.PreviousState.Id);
+            };
+            // you can subscribe for machine done event
+            interpreter.OnStateMachineDone += (sender, args) => {
+                Console.WriteLine("State machine finalized!");
+            };
         }
     }
 }
