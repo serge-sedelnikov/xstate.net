1855321c6ecc20e4eca0ae5c7f7a112974e370e1
Sergey Sedelnikov <sergey.sedelnikov@yahoo.com>
2021-01-06 18:44:09 +0200

    clean up code

--- demo-console-app/Program.cs
+++ demo-console-app/Program.cs
@@ -29,34 +29,5 @@ namespace demo_console_app
             }
 
         }
-
-        void Demo()
-        {
-            // create state. Each state has a unique ID
-            State myState1 = new State("myState1");
-            State myState2 = new State("myState2");
-            State myState3 = new State("myState3");
-            State myState4 = new State("myState4");
-
-            // create state machine
-            // provide ID, name, and initial state ID
-            StateMachine machine = new StateMachine("myMachine1", "My Machine", "myState1");
-            // fill up states
-            machine.States = new []{
-                myState1, myState2, myState3, myState4
-            };
-
-            Interpreter interpreter = new Interpreter(machine);
-            interpreter.StartStateMachine();
-            // you can subscribe for state changed event
-            interpreter.OnStateChanged += (sender, args) => {
-                Console.WriteLine("Current state: " + args.State.Id);
-                Console.WriteLine("Previous state: " + args.PreviousState.Id);
-            };
-            // you can subscribe for machine done event
-            interpreter.OnStateMachineDone += (sender, args) => {
-                Console.WriteLine("State machine finalized!");
-            };
-        }
     }
 }
