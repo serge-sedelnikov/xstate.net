2bd75e595f8f7f5465ed9491b204b8777736e8c7
Sergey Sedelnikov <sergey.sedelnikov@yahoo.com>
2021-01-07 22:14:03 +0200

    documentation for actions and async cancel

--- demo-console-app/Program.cs
+++ demo-console-app/Program.cs
@@ -15,11 +15,14 @@ namespace demo_console_app
             Console.WriteLine("Demo application to demonstrate the state machien engine");
 
             // ==================== TRAFFIC LIGHT DEMO ============================
-            TrafficLight.TrafficLightDemoRunner.Run();
+            // TrafficLight.TrafficLightDemoRunner.Run();
 
             // ==================== MQTT DEMO ============================
             //MqttDemo.MqttDemoRunner.Run();
 
+
+            Demo();
+            
             // never exit the program
             while (true)
             {
@@ -27,7 +30,39 @@ namespace demo_console_app
                 // is executed and interpreted in own thread.
                 Console.ReadLine();
             }
+        }
 
+        static async Task Demo() 
+        {
+            // starting the stopwatch
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            // creating the state with three on enter actions
+            // all of them are asyncronouse.
+            State state1 = new State("myState")
+            .WithActionOnEnter(async () => {
+                // in each action we wait for 10 seconds
+                await Task.Delay(10000);
+                Console.WriteLine("Action 1");
+                Console.WriteLine(watch.Elapsed);
+            })
+            .WithActionOnEnter(async () => {
+                await Task.Delay(10000);
+                Console.WriteLine("Action 2");
+                Console.WriteLine(watch.Elapsed);
+            })
+            .WithActionOnEnter(async () => {
+                await Task.Delay(10000);
+                Console.WriteLine("Action 3");
+                Console.WriteLine(watch.Elapsed);
+            });
+
+            // starting the state machine
+            var machine = new StateMachine("myMachine", "my machine", "myState", state1);
+            var interpreter = new Interpreter(machine);
+            // waiting untl machine is over
+            await interpreter.StartStateMachineAsync();
         }
     }
 }
